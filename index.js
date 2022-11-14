5d106a49565fe43119cb194a23b0ea2355d9e4f8
Margarita <m.novolokova@gmail.com>
2022-11-14 20:43:56 +0200

    Attempt to make deterministic and pure functions

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const MAX_NUM = 35;
 let attempts = 4;
 let multiple = 6;
 
-
 // for
 
 for (let i = 1; i < attempts; i++) {
@@ -23,7 +22,7 @@ for (let i = 1; i < attempts; i++) {
   console.log(`try again: ${i} attempt`);
 }
 
-// while
+// // while
 
 let count = 1;
 while (count < attempts) {
@@ -31,7 +30,7 @@ while (count < attempts) {
 
   if (inputUser > MIN_NUM && inputUser < MAX_NUM) {
     if (inputUser % multiple === 0) {
-      console.log('This is right');
+      console.log("This is right");
       break;
     }
   }
@@ -39,12 +38,74 @@ while (count < attempts) {
   console.log(`try again: ${count} attempt`);
 }
 
+/*********************************************/
+// СПРОБА зробити детерміновані та чисті функції
 
+/**
+ *
+ * @returns {string}
+ */
+const getUserInput = function () {
+  return prompt("Enter Number, you have 3 attempts");
+};
 
+/**
+ *
+ * @param {string} value
+ * @returns {number}
+ */
+const checkInputUser = function (value) {
+  return Number(value);
+};
 
+/**
+ *
+ */
+const showGriitengs = function () {
+  console.log("This is right");
+};
 
+/**
+ *
+ * @param {number} value
+ */
+const showAttempts = function (value) {
+  console.log(`try again: ${value} attempt`);
+};
 
+/**
+ *
+ * @param {number} value
+ * @param {number} MIN_NUM
+ * @param {number} MAX_NUM
+ * @returns {number, boolean}
+ */
+const checkRange = function (value, MIN_NUM = 15, MAX_NUM = 35) {
+  if (value > MIN_NUM && value < MAX_NUM) {
+    return value;
+  }
+  return false;
+};
 
+/**
+ *
+ * @param {number} value_1
+ * @param {number} value_2
+ * @returns {boolean}
+ */
+const checkMultiplicity = function (value_1, value_2 = 6) {
+  return value_1 % value_2 === 0;
+};
 
+debugger;
+for (let i = 1; i < attempts; i++) {
+  const inputUser = checkInputUser(getUserInput());
 
-
+  if (checkRange(inputUser)) {
+    if (checkMultiplicity(inputUser)) {
+      showGriitengs();
+      break;
+    }
+  }
+  showAttempts(i);
+}
